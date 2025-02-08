759a0feb7486066ed4a6cd927fcaed93cd9f63b8
Deepak Rizal <deepak2000rizal@gmail.com>
2025-02-08 17:27:43 +0530

    feat:add navbar UI with logo and sign-in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section>
+      <nav className="flex justify-between px-10 py-5">
+        <div className="logo flex items-center gap-1 justify-center">
+          <Image
+            src="/convosync-logo.webp"
+            alt="ConvoSync Logo"
+            width={40}
+            height={10}
+            className="rounded-full"
+          />
+          <h2 className=" font-semibold ">convosync</h2>
+        </div>
+
+        <div className="">
+          <button className="btn px-10 py-1 rounded-md">Sign in</button>
+        </div>
+      </nav>
+    </section>
+  );
 }
