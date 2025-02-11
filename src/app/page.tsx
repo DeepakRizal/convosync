529326d6ad8505d834a7a304eadc4202c507051c
Deepak Rizal <deepak2000rizal@gmail.com>
2025-02-11 11:20:35 +0530

    Resolve hydration mismatch due to browser extension and made some small html structural changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import SignIn from "@/components/signIn/SignIn";
 import Image from "next/image";
 import { IoIosSearch } from "react-icons/io";
 import { IoAdd } from "react-icons/io5";
 
+import SignIn from "@/components/signIn/SignIn";
+
 export default function Home() {
   return (
-    <section className=" w-full">
+    <main className=" w-full">
       <header className=" max-w-[1200px]  mx-auto">
         <div className="flex justify-between py-5 items-center ">
           <div className="logo flex items-center gap-1 justify-center">
@@ -13,19 +14,19 @@ export default function Home() {
               src="/convosync-logo.webp"
               alt="ConvoSync Logo"
               width={40}
-              height={10}
+              height={40}
               className="rounded-full"
             />
             <h2 className=" font-semibold ">convosync</h2>
           </div>
 
-          <div className="">
+          <div>
             <SignIn />
           </div>
         </div>
         <h1 className=" text-center px-20 text-3xl">Convosync Community</h1>
       </header>
-      <main className="max-w-[1200px] mx-auto">
+      <section className="max-w-[1200px] mx-auto">
         <div
           className="flex gap-5 mt-16
         "
@@ -46,7 +47,7 @@ export default function Home() {
             </button>
           </div>
         </div>
-      </main>
-    </section>
+      </section>
+    </main>
   );
 }
